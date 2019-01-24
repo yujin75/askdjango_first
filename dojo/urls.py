57a1b9a709d3dc868b1ae860a0ed30f6b0431883
yujin75 <wkddbwls514@naver.com>
2019-01-25 02:36:41 +0900

    ep4)클래스 기반 뷰 (CBV) 의 4가지 패턴

--- dojo/urls.py
+++ dojo/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from django.urls import path, re_path
 
 from dojo import views
+from . import views_cbv
 
 urlpatterns = [
     re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
@@ -11,4 +12,9 @@ urlpatterns = [
     path('list2/', views.post_list2),
     path('list3/', views.post_list3),
     path('excel/', views.excel_download),
+
+    path('cbv/list1/', views_cbv.post_list1),
+    path('cbv/list2/', views_cbv.post_list2),
+    path('cbv/list3/', views_cbv.post_list3),
+    path('cbv/excel/', views_cbv.excel_download),
 ]
