7da6b101009e5d74f0b1103463de701dafdadd94
yujin75 <wkddbwls514@naver.com>
2019-01-25 02:14:24 +0900

    ep4)함수 기반 뷰 (FBV) 의 4가지 패턴

--- dojo/urls.py
+++ dojo/urls.py
@@ -7,4 +7,8 @@ from dojo import views
 urlpatterns = [
     re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     re_path(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello),
+    path('list1/', views.post_list1),
+    path('list2/', views.post_list2),
+    path('list3/', views.post_list3),
+    path('excel/', views.excel_download),
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -1,10 +1,45 @@
 #dojo/views.py
-from django.http import HttpResponse
+import os
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 def mysum(request, numbers):
     result = sum(map(lambda s:int(s or 0), numbers.split("/")))
     return HttpResponse(result)
 
+
 def hello(request, name, age):
     return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name, age))
+
+
+def post_list1(request):
+    name = '공유'
+    return HttpResponse('''
+    <h1>AskDjango</h1>
+    <p>{name}</p>
+    <p>여러분의 파이썬&장고 페이스메이커가 되어드리겠습니다.</p>
+    '''.format(name=name))
+
+
+def post_list2(request):
+    name = '공유'
+    return render(request, 'dojo/post_list.html', {'name':name})
+
+
+def post_list3(request):
+    return JsonResponse({
+        'message': '안녕 파이썬&장고',
+        'items': ['파이썬', '장고', 'Celery', 'Azure', 'AWS'],
+    }, json_dumps_params={'ensure_ascii' : False})
+
+
+def excel_download(request):
+    # filepath = 'c:\dev/askdjango/tests.xls'   #file path 지정1-절대 경로 지정
+    filepath = os.path.join(settings.BASE_DIR, 'tests.xls') #file path 지정2-현재 프로젝트의 최상위 디렉토리 주소 지정
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
