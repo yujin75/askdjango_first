4e64d63bdcd1eaa4c83f803f2532094f9aa5081e
yujin75 <wkddbwls514@naver.com>
2019-01-25 23:00:14 +0900

    ep5)tags, lnglat 필드 추가/필드 옵션 지정

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,18 @@
 #blog/models.py
+import re
+from django.core.exceptions import ValidationError
 from django.db import models
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목을 입력해주세요. 최대 100자 내외')
     content = models.TextField(verbose_name='내용')
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, blank=True,
+                              validators=[lnglat_validator],
+                              help_text='경도/위도 포맷으로 입력')
     created_at = models.DateTimeField(auto_now_add=True)    #길이 제한 있는 문자열
     updated_at = models.DateTimeField(auto_now=True)    #길이 제한 없는 문자열
