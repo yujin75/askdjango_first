928981c8edfa27be93a79b264c846c292a434cb9
yujin75 <wkddbwls514@naver.com>
2019-01-27 22:48:28 +0900

    ep14)url reverse 샘플코드 추가

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 from django.urls import path, re_path
 
 urlpatterns = [
-    path('', views.post_list),
-    re_path(r'^(?P<id>\d+)/$', views.post_detail),
+    path('', views.post_list, name='post_list'),
+    re_path(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
 ]
