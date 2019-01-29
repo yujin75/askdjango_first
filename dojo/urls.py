d9a2850a0789afe8e2408cd0cecdf809bd8ea6ed
yujin75 <wkddbwls514@naver.com>
2019-01-29 22:40:07 +0900

    ep22)dojo.views.post_edit 뷰 구현

--- dojo/urls.py
+++ dojo/urls.py
@@ -8,6 +8,7 @@ app_name = 'dojo'
 
 urlpatterns = [
     path('new/', views.post_new),
+    re_path(r'^(?P<id>\d+)/edit/$', views.post_edit),
 
     re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum, name='mysum'),
     re_path(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello, name='hello'),
--- dojo/views.py
+++ dojo/views.py
@@ -2,7 +2,7 @@
 import os
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from dojo.forms import PostForm
 from dojo.models import Post
 
@@ -17,6 +17,21 @@ def post_new(request):
             return redirect('/dojo/') #namespace:name
     else:
         form = PostForm()
+    return render(request, 'dojo/post_form.html', {
+        'form': form,
+        })
+
+def post_edit(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.ip = request.META['REMOTE_ADDR']
+            post.save()
+            return redirect('/dojo/') #namespace:name
+    else:
+        form = PostForm(instance=post)
     return render(request, 'dojo/post_form.html', {
         'form': form,
         })
