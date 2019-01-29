ce50248604535184b4db42db1b78319ae3dfaab1
yujin75 <wkddbwls514@naver.com>
2019-01-29 20:54:13 +0900

    ep21)dojo.views.post_new 뷰 구현

--- dojo/views.py
+++ dojo/views.py
@@ -2,7 +2,42 @@
 import os
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from dojo.forms import PostForm
+from dojo.models import Post
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            # 방법1)
+            # post = Post()
+            # post.title = form.cleaned_data['title']
+            # post.content = form.cleaned_data['content']
+            # post.save()
+
+            # 방법2)
+            '''
+            post = Post(title = form.cleaned_data['title'],
+                        content= form.cleaned_data['content'])
+            '''
+
+            # 방법3)
+            '''
+            post = Post.objects.create(title = form.cleaned_data['title'],
+                                       content = form.cleaned_data['content'])
+            '''
+
+            # 방법4)
+            post = Post.objects.create(**form.cleaned_data)
+            return redirect('/dojo/') #namespace:name
+    else:
+        form = PostForm()
+
+    return render(request, 'dojo/post_form.html', {
+        'form': form,
+        })
 
 def mysum(request, numbers):
     result = sum(map(lambda s:int(s or 0), numbers.split("/")))
