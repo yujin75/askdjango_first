8a2a5ba9c41765e035c580d9c9a942923ed56cd3
yujin75 <wkddbwls514@naver.com>
2019-02-04 19:52:54 +0900

    ep33)로그인/로그아웃 구현

--- accounts/models.py
+++ accounts/models.py
@@ -8,3 +8,4 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
+
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,16 @@
+from django.conf import settings
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
 
+
+
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login_form.html')
+         , name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=settings.LOGIN_URL), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
