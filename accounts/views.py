3ce17049cf50e1b60ced4ca99ec92c850e419101
yujin75 <wkddbwls514@naver.com>
2019-02-04 20:06:35 +0900

    ep33)프로필 뷰에 login_required 장식자 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 #accounts/views.py
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
@@ -16,6 +17,6 @@ def signup(request):
         'form': form,
     })
 
-
+@login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
