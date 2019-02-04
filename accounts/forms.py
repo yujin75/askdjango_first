a28d9278531069e15a939665ee6a17803b799a8d
yujin75 <wkddbwls514@naver.com>
2019-02-04 20:35:07 +0900

    ep34)퀴즈를 맞춰야 로그인

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from accounts.models import Profile
 
@@ -19,3 +19,13 @@ class SignupForm(UserCreationForm):
             address = self.cleaned_data['address'])
 
         return user
+
+
+class LoginForm(AuthenticationForm):
+    answer = forms.IntegerField(label='3+3=?')
+
+    def clean_answer(self):
+        answer = self.cleaned_data.get('answer', None)
+        if answer != 6:
+            raise forms.ValidationError('mismatched!')
+        return answer
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .forms import LoginForm
 
 app_name = 'accounts'
 
@@ -9,7 +10,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login_form.html')
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login_form.html', authentication_form=LoginForm)
          , name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page=settings.LOGIN_URL), name='logout'),
     path('profile/', views.profile, name='profile'),
